fecdd1dc5e846b5aab23a1a063ed5410e0db831a
vasantymarshalgmail.com <vasantymarshal@gamil.com>
2024-05-27 22:36:40 +0200

    trying commit

--- main.py
+++ main.py
@@ -1,2 +1,24 @@
+import random
+
 class product:
-    def__init__(self, ):
+
+    def __init__(self, name, price, quantity):
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+        self.active = quantity > 0
+    def
+
+
+bose = Product("Bose QuietComfort Earbuds", price=250, quantity=500)
+mac = Product("MacBook Air M2", price=1450, quantity=100)
+
+print(bose.buy(50))
+print(mac.buy(100))
+print(mac.is_active())
+
+bose.show()
+mac.show()
+
+bose.set_quantity(1000)
+bose.show()
